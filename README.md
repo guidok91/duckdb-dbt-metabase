15296ff47e7f181e2d31d17c05858c1beda414ca
guidok91 <guidokosloff@gmail.com>
2023-09-23 00:22:47 -0300

    Use macro to convert time

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Run the following commands in order:
 1. `make build` to build the Docker image locally.
 2. `make run` to start a local container.
 3. `make departures-download` to download the plane departures data from the rest API into a local JSON dataset (*).
-4. `make dbt-run` to run the dbt models that process the data.
-5. `make dbt-test` to run the tests. 
+4. `make dbt-deps` to install the required deps.
+5. `make dbt-run` to run the dbt models that process the data.
+6. `make dbt-test` to run the tests. 
 
 You can then run `make duckdb` to play around with the datasets in the DuckDB console.
 
--- models/departures/departures_curated.sql
+++ models/departures/departures_curated.sql
@@ -3,12 +3,12 @@ SELECT
     flight_iata AS flight_number_iata_code,
     dep_iata AS departure_airport_iata_code,
     dep_terminals AS departure_terminals,
-    CAST(dep_time || ':00' AS TIME) AS departure_time_local,
-    CAST(dep_time_utc || ':00' AS TIME) AS departure_time_utc,
+    {{ convert_time('dep_time') }} AS departure_time_local,
+    {{ convert_time('dep_time_utc') }}  AS departure_time_utc,
     arr_iata AS arrival_airport_iata_code,
     arr_terminals AS arrival_terminals,
-    CAST(arr_time || ':00' AS TIME) AS arrival_time_local,
-    CAST(arr_time_utc || ':00' AS TIME) AS arrival_time_utc,
+    {{ convert_time('arr_time') }}  AS arrival_time_local,
+    {{ convert_time('arr_time_utc') }}  AS arrival_time_utc,
     days AS flight_days,
     CURRENT_TIMESTAMP AS processed_timestamp
 FROM
