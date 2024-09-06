254ad54341bfc7cd653565dea0c12dc0017584a8
guidok91 <guidokosloff@gmail.com>
2024-09-06 10:19:26 +0200

    Add comments about generate_schema_name macro

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,3 +1,6 @@
+-- Override the built-in macro so that the specified schema is used as is
+-- instead of appending the schema to the target schema (e.g. `curated` instead of `main_curated`)
+-- See https://docs.getdbt.com/docs/build/custom-schemas
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
